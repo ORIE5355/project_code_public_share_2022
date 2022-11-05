8cd89d094372264eb7c4cbadd2ff7e13220df31a
Nikhil Garg <nkgar6@gmail.com>
2022-11-05 16:15:27 -0400

    updated for Part 2 as well

--- algopricing_opy/MultiAgentEnv_algopricing.py
+++ algopricing_opy/MultiAgentEnv_algopricing.py
@@ -1,158 +1,158 @@
 # coding: UTF-8
 import sys
-l11l1ll_opy_ = sys.version_info [0] == 2
-l1ll1l1_opy_ = 2048
-l11ll1l_opy_ = 7
-def l1llll1_opy_ (l111ll1_opy_):
-    global l11lll1_opy_
-    l111l1l_opy_ = ord (l111ll1_opy_ [-1])
-    l1l111_opy_ = l111ll1_opy_ [:-1]
-    l1ll11_opy_ = l111l1l_opy_ % len (l1l111_opy_)
-    l1l1ll_opy_ = l1l111_opy_ [:l1ll11_opy_] + l1l111_opy_ [l1ll11_opy_:]
-    if l11l1ll_opy_:
-        l111l1_opy_ = unicode () .join ([unichr (ord (char) - l1ll1l1_opy_ - (l1lll1_opy_ + l111l1l_opy_) % l11ll1l_opy_) for l1lll1_opy_, char in enumerate (l1l1ll_opy_)])
+l1l1l11_opy_ = sys.version_info [0] == 2
+l11l11_opy_ = 2048
+l1lll11_opy_ = 7
+def l1111ll_opy_ (l11_opy_):
+    global l111l1l_opy_
+    l1l1_opy_ = ord (l11_opy_ [-1])
+    l1l11l1_opy_ = l11_opy_ [:-1]
+    l1llll1l_opy_ = l1l1_opy_ % len (l1l11l1_opy_)
+    l111ll1_opy_ = l1l11l1_opy_ [:l1llll1l_opy_] + l1l11l1_opy_ [l1llll1l_opy_:]
+    if l1l1l11_opy_:
+        l1ll1l_opy_ = unicode () .join ([unichr (ord (char) - l11l11_opy_ - (l11lll1_opy_ + l1l1_opy_) % l1lll11_opy_) for l11lll1_opy_, char in enumerate (l111ll1_opy_)])
     else:
-        l111l1_opy_ = str () .join ([chr (ord (char) - l1ll1l1_opy_ - (l1lll1_opy_ + l111l1l_opy_) % l11ll1l_opy_) for l1lll1_opy_, char in enumerate (l1l1ll_opy_)])
-    return eval (l111l1_opy_)
-# import l1l1111_opy_
-# from l1l1111_opy_ import spaces
-# from l1l1111_opy_.l1111l1_opy_.l1111l_opy_ import l11l1l1_opy_
+        l1ll1l_opy_ = str () .join ([chr (ord (char) - l11l11_opy_ - (l11lll1_opy_ + l1l1_opy_) % l1lll11_opy_) for l11lll1_opy_, char in enumerate (l111ll1_opy_)])
+    return eval (l1ll1l_opy_)
+# import l111l1_opy_
+# from l111l1_opy_ import spaces
+# from l111l1_opy_.l1lll1l1_opy_.l1ll1l1_opy_ import l1ll11_opy_
 import numpy as np
 import copy
 import random
 import matplotlib.pyplot as plt
-import seaborn as l111l_opy_
+import seaborn as l1lllll_opy_
 import pandas as pd
 from cryptography.fernet import Fernet
 import pickle
-# l11l11_opy_ l111111_opy_ from here: l11l1_opy_://l1l1l11_opy_.com/l1lll11_opy_/l1_opy_-l1l_opy_-l1111l1_opy_/l1lll_opy_/l11l1l_opy_/l1_opy_/l1l1lll_opy_.py
-# also l1l1ll1_opy_: l11l1_opy_://l1l1l_opy_.ai-l1ll1ll_opy_.net/l1l11_opy_-a-l1lllll_opy_-l1l1111_opy_-l1lll11_opy_-l1l1lll_opy_-for-l11_opy_-l1ll1_opy_/
-def l111_opy_(df, list_of_columns, l11111_opy_):
-    obj = Fernet(l11111_opy_)
+# l11l_opy_ l111l_opy_ from here: l1l111l_opy_://l1l1l_opy_.com/l11llll_opy_/l1ll1ll_opy_-l1llll11_opy_-l1lll1l1_opy_/blob/l11lll_opy_/l1ll1ll_opy_/l1l1ll_opy_.py
+# also l11l1l_opy_: l1l111l_opy_://l1111l_opy_.ai-l1l1lll_opy_.l1lll1l_opy_/l1ll_opy_-a-l1lllll1_opy_-l111l1_opy_-l11llll_opy_-l1l1ll_opy_-for-l11l1ll_opy_-l111ll_opy_/
+def l1l11l_opy_(df, list_of_columns, l11ll11_opy_):
+    obj = Fernet(l11ll11_opy_)
     for col in list_of_columns:
         df[col] = df[col].apply(lambda x: obj.encrypt(
-            bytes(str(x).encode(l1llll1_opy_ (u"ࠫࡺࡺࡦ࠮࠺ࠪࠀ")).hex(), l1llll1_opy_ (u"ࠬࡻࡴࡧ࠯࠻ࠫࠁ"))))
+            bytes(str(x).encode(l1111ll_opy_ (u"ࠫࡺࡺࡦ࠮࠺ࠪࠀ")).hex(), l1111ll_opy_ (u"ࠬࡻࡴࡧ࠯࠻ࠫࠁ"))))
     return df
-def l11l111_opy_(df, list_of_columns, l11111_opy_):
-    obj = Fernet(l11111_opy_)
+def l1111l1_opy_(df, list_of_columns, l11ll11_opy_):
+    obj = Fernet(l11ll11_opy_)
     for col in list_of_columns:
         df[col] = df[col].apply(lambda x: float(bytes.fromhex(
-            obj.decrypt(bytes(x[2:-1], l1llll1_opy_ (u"࠭ࡵࡵࡨ࠰࠼ࠬࠂ"))).decode().strip())))
+            obj.decrypt(bytes(x[2:-1], l1111ll_opy_ (u"࠭ࡵࡵࡨ࠰࠼ࠬࠂ"))).decode().strip())))
     return df
-def l11l11l_opy_(l1ll_opy_, l11111_opy_):
-    df = pd.read_csv(l1ll_opy_)
-    df.index = df[l1llll1_opy_ (u"ࠧࡖࡰࡱࡥࡲ࡫ࡤ࠻ࠢ࠳ࠫࠃ")].values
-    del df[l1llll1_opy_ (u"ࠨࡗࡱࡲࡦࡳࡥࡥ࠼ࠣ࠴ࠬࠄ")]
-    if l11111_opy_ is not None:
-        df = l11l111_opy_(df, df.columns.tolist(), l11111_opy_)
+def l111lll_opy_(l111l11_opy_, l11ll11_opy_):
+    df = pd.read_csv(l111l11_opy_)
+    if l11ll11_opy_ is not None:
+        df = l1111l1_opy_(df, df.columns.tolist(), l11ll11_opy_)
+    df.index = df[l1111ll_opy_ (u"ࠧࡶࡵࡨࡶࡤ࡯࡮ࡥࡧࡻࠫࠃ")].values
+    del df[l1111ll_opy_ (u"ࠨࡷࡶࡩࡷࡥࡩ࡯ࡦࡨࡼࠬࠄ")]
     return df
-def l1l1_opy_(filename):
-    with open(filename, l1llll1_opy_ (u"ࠤࡵࡦࠧࠅ")) as l1l1l1_opy_:
-        loaded = pickle.load(l1l1l1_opy_)
+def l1l1l1_opy_(filename):
+    with open(filename, l1111ll_opy_ (u"ࠤࡵࡦࠧࠅ")) as l1l11ll_opy_:
+        loaded = pickle.load(l1l11ll_opy_)
     return loaded
 class MultiAgentEnv_algopricing(object):
-    def __init__(self, params, l11lll_opy_, l1111ll_opy_, l1l1l1l_opy_=2, l1111_opy_=None, l1llllll_opy_=None):
+    def __init__(self, params, l111111_opy_, l1l1l1l_opy_, l11l1l1_opy_=2, l1ll11l_opy_=None, l11ll1l_opy_=None):
         self.time = 0
         self.cumulative_buyer_utility = 0
-        self.l1llll1l_opy_ = params[l1llll1_opy_ (u"ࠥࡲࡤ࡯ࡴࡦ࡯ࡶࠦࠆ")]
+        self.l1lll1ll_opy_ = params[l1111ll_opy_ (u"ࠥࡲࡤ࡯ࡴࡦ࡯ࡶࠦࠆ")]
+        self.l11l1l1_opy_ = l11l1l1_opy_
         self.l1l1l1l_opy_ = l1l1l1l_opy_
-        self.l1111ll_opy_ = l1111ll_opy_
-        self.l11lll_opy_ = l11lll_opy_
-        self.l111lll_opy_ = [0 for _ in range(self.l1l1l1l_opy_)]
-        self.l11111l_opy_ = [[] for _ in range(self.l1l1l1l_opy_)]
-        self.l11ll11_opy_ = []
-        self.l1111_opy_ = l1111_opy_
-        self.l1l11l1_opy_ = l1llllll_opy_
-        self.l1llll_opy_ = None
-        self.l1l11ll_opy_ = None
-        self.l1l111l_opy_ = bytes(
-            l1llll1_opy_ (u"ࠫ࠵࠶࠰࠱࠲࠳࠴࠵࠶࠰࠱࠲࠳࠴࠵࠶ࡩࡩࡱࡳࡩࡾࡵࡵࡥࡱࡱࡸࡰࡴ࡯ࡸ࡯ࡼࡷࡪࡩࡲࡦࡶ࡮ࡩࡾࡃࠧࠇ"), l1llll1_opy_ (u"ࠬࡻࡴࡧ࠯࠻ࠫࠈ"))
-        self._1llll11_opy_()
-    def _1llll11_opy_(self):
-        if self.l1111_opy_ is None:
+        self.l111111_opy_ = l111111_opy_
+        self.l1l1ll1_opy_ = [0 for _ in range(self.l11l1l1_opy_)]
+        self.l1ll1_opy_ = [[] for _ in range(self.l11l1l1_opy_)]
+        self.l11l11l_opy_ = []
+        self.l1ll11l_opy_ = l1ll11l_opy_
+        self.l1llll_opy_ = l11ll1l_opy_
+        self.l11111l_opy_ = None
+        self.l11l111_opy_ = None
+        self.l1l11_opy_ = bytes(
+            l1111ll_opy_ (u"ࠫ࠵࠶࠰࠱࠲࠳࠴࠵࠶࠰࠱࠲࠵࠴࠷࠸ࡩࡩࡱࡳࡩࡾࡵࡵࡥࡱࡱࡸࡰࡴ࡯ࡸ࡯ࡼࡷࡪࡩࡲࡦࡶ࡮ࡩࡾࡃࠧࠇ"), l1111ll_opy_ (u"ࠬࡻࡴࡧ࠯࠻ࠫࠈ")) #l1l11_opy_ for l1l1111_opy_ with l1l_opy_
+        self._l_opy_()
+    def _l_opy_(self):
+        if self.l1ll11l_opy_ is None:
             return
         else:
-            self.l1llll_opy_ = l11l11l_opy_(
-                self.l1111_opy_, self.l1l111l_opy_)
-            self.l1l11ll_opy_ = l11l11l_opy_(
-                self.l1l11l1_opy_, self.l1l111l_opy_)
+            self.l11111l_opy_ = l111lll_opy_(
+                self.l1ll11l_opy_, self.l1l11_opy_)
+            self.l11l111_opy_ = l111lll_opy_(
+                self.l1llll_opy_, self.l1l11_opy_)
     def get_current_customer(self):
-        assert self.time <= len(self.l11ll11_opy_)
-        if len(self.l11ll11_opy_) == self.time:
-            if self.l1111ll_opy_ == 1:
-                l1lll1l_opy_ = l111l11_opy_ = list(abs(np.random.normal(5, 5, 1)))
-            elif self.l1llll_opy_ is None:
-                l1lll1l_opy_ = [0, 0, 0]
-                l111l11_opy_ = [random.random() * 2 for _ in range(self.l1llll1l_opy_)]
+        assert self.time <= len(self.l11l11l_opy_)
+        if len(self.l11l11l_opy_) == self.time:
+            if self.l1l1l1l_opy_ == 1:
+                l11l1_opy_ = l1lll_opy_ = list(abs(np.random.normal(5, 5, 1)))
+            elif self.l11111l_opy_ is None:
+                l11l1_opy_ = [0, 0, 0]
+                l1lll_opy_ = [random.random() * 2 for _ in range(self.l1lll1ll_opy_)]
             else:
-                l1ll1l_opy_ = random.choice(
-                    self.l1llll_opy_.index.values)
-                l1lll1l_opy_ = self.l1llll_opy_.loc[l1ll1l_opy_].values
-                l111l11_opy_ = [
-                    self.l1l11ll_opy_.loc[l1ll1l_opy_, l1llll1_opy_ (u"࠭ࡩࡵࡧࡰࡿࢂࡼࡡ࡭ࡷࡤࡸ࡮ࡵ࡮ࡴࠩࠉ").format(l11llll_opy_)] for l11llll_opy_ in range(self.l1llll1l_opy_)
+                l111_opy_ = random.choice(
+                    self.l11111l_opy_.index.values)
+                l11l1_opy_ = self.l11111l_opy_.loc[l111_opy_].values
+                l1lll_opy_ = [
+                    self.l11l111_opy_.loc[l111_opy_, l1111ll_opy_ (u"࠭ࡩࡵࡧࡰࡿࢂࡼࡡ࡭ࡷࡤࡸ࡮ࡵ࡮ࠨࠉ").format(l11ll_opy_)] for l11ll_opy_ in range(self.l1lll1ll_opy_)
                 ]
-            self.l11ll11_opy_.append((l1lll1l_opy_, l111l11_opy_))
+            self.l11l11l_opy_.append((l11l1_opy_, l1lll_opy_))
         else:
-            l1lll1l_opy_, l111l11_opy_ = self.l11ll11_opy_[self.time]
-        return l1lll1l_opy_, l111l11_opy_
-    def get_current_state_customer_to_send_agents(self, l11ll1_opy_=None):
-        if l11ll1_opy_ is None:
-            l11ll1_opy_ = (np.nan, np.nan, [[np.nan for _ in range(self.l1llll1l_opy_)], [np.nan for _ in range(self.l1llll1l_opy_)]])
-        l1llll_opy_, l1lllll1_opy_ = self.get_current_customer()
-        state = self.l111lll_opy_
-        return l1llll_opy_, l11ll1_opy_, state
-    def step(self, ll_opy_):
+            l11l1_opy_, l1lll_opy_ = self.l11l11l_opy_[self.time]
+        return l11l1_opy_, l1lll_opy_
+    def get_current_state_customer_to_send_agents(self, l1ll111_opy_=None):
+        if l1ll111_opy_ is None:
+            l1ll111_opy_ = (np.nan, np.nan, [[np.nan for _ in range(self.l1lll1ll_opy_)], [np.nan for _ in range(self.l1lll1ll_opy_)]])
+        l11111l_opy_, l11111_opy_ = self.get_current_customer()
+        state = self.l1l1ll1_opy_
+        return l11111l_opy_, l1ll111_opy_, state
+    def step(self, l1llllll_opy_):
         eps = 1e-7
-        _, l111l11_opy_ = self.get_current_customer()
-        l111ll_opy_ = 0
-        l1ll111_opy_ = -1
-        l11l_opy_ = -1
-        for item in range(self.l1llll1l_opy_):
-            value = l111l11_opy_[item]
-            for l1l11l_opy_ in range(self.l1l1l1l_opy_):
-                util = value - ll_opy_[l1l11l_opy_][item]
-                if util >= 0 and util + (random.random() - 0.5) * eps > l111ll_opy_:
-                    l111ll_opy_ = util
-                    l1ll111_opy_ = item
-                    l11l_opy_ = l1l11l_opy_
-        if l11l_opy_ >= 0:
-            self.l111lll_opy_[l11l_opy_] += ll_opy_[
-                l11l_opy_
-            ][l1ll111_opy_]
-            self.cumulative_buyer_utility += l111ll_opy_
-            l11ll1_opy_ = (
-                l1ll111_opy_,
-                l11l_opy_,
-                ll_opy_,
+        _, l1lll_opy_ = self.get_current_customer()
+        l1_opy_ = 0
+        l11ll1_opy_ = -1
+        l1111_opy_ = -1
+        for item in range(self.l1lll1ll_opy_):
+            value = l1lll_opy_[item]
+            for l1l111_opy_ in range(self.l11l1l1_opy_):
+                util = value - l1llllll_opy_[l1l111_opy_][item]
+                if util >= 0 and util + (random.random() - 0.5) * eps > l1_opy_:
+                    l1_opy_ = util
+                    l11ll1_opy_ = item
+                    l1111_opy_ = l1l111_opy_
+        if l1111_opy_ >= 0:
+            self.l1l1ll1_opy_[l1111_opy_] += l1llllll_opy_[
+                l1111_opy_
+            ][l11ll1_opy_]
+            self.cumulative_buyer_utility += l1_opy_
+            l1ll111_opy_ = (
+                l11ll1_opy_,
+                l1111_opy_,
+                l1llllll_opy_,
             )
         else:
-            l11ll1_opy_ = (np.nan, np.nan, ll_opy_)
-        for l1l11l_opy_ in range(self.l1l1l1l_opy_):
-            self.l11111l_opy_[l1l11l_opy_].append(
-                self.l111lll_opy_[l1l11l_opy_])
+            l1ll111_opy_ = (np.nan, np.nan, l1llllll_opy_)
+        for l1l111_opy_ in range(self.l11l1l1_opy_):
+            self.l1ll1_opy_[l1l111_opy_].append(
+                self.l1l1ll1_opy_[l1l111_opy_])
         self.time += 1
-        return self.get_current_state_customer_to_send_agents(l11ll1_opy_)
+        return self.get_current_state_customer_to_send_agents(l1ll111_opy_)
     def reset(self):
         self.time = 0
         self.cumulative_buyer_utility = 0
-        self.l111lll_opy_ = [0 for _ in range(self.l1l1l1l_opy_)]
-        self.l11111l_opy_ = [[] for _ in range(self.l1l1l1l_opy_)]
-        self.l11ll11_opy_ = []
-        self._1llll11_opy_()
-    def render(self, l1ll11l_opy_=False, mode=l1llll1_opy_ (u"ࠢࡩࡷࡰࡥࡳࠨࠊ"), close=False, l11ll_opy_=10):
-        if self.time % l11ll_opy_ == 0:
-            if l1ll11l_opy_:
+        self.l1l1ll1_opy_ = [0 for _ in range(self.l11l1l1_opy_)]
+        self.l1ll1_opy_ = [[] for _ in range(self.l11l1l1_opy_)]
+        self.l11l11l_opy_ = []
+        self._l_opy_()
+    def render(self, l1llll1_opy_=False, mode=l1111ll_opy_ (u"ࠢࡩࡷࡰࡥࡳࠨࠊ"), close=False, l1lll1_opy_=10):
+        if self.time % l1lll1_opy_ == 0:
+            if l1llll1_opy_:
                 plt.close()
-            for l1l11l_opy_ in range(self.l1l1l1l_opy_):
-                name = l1llll1_opy_ (u"ࠣࡃࡪࡩࡳࡺࠠࡼࡿ࠽ࠤࢀࢃࠢࠋ").format(l1l11l_opy_, self.l11lll_opy_[l1l11l_opy_])
+            for l1l111_opy_ in range(self.l11l1l1_opy_):
+                name = l1111ll_opy_ (u"ࠣࡃࡪࡩࡳࡺࠠࡼࡿ࠽ࠤࢀࢃࠢࠋ").format(l1l111_opy_, self.l111111_opy_[l1l111_opy_])
                 plt.plot(
                     list(range(self.time)),
-                    self.l11111l_opy_[l1l11l_opy_],
+                    self.l1ll1_opy_[l1l111_opy_],
                     label=name,
                 )
             plt.legend(frameon=False)
-            plt.xlabel(l1llll1_opy_ (u"ࠤࡗ࡭ࡲ࡫ࠢࠌ"))
-            plt.ylabel(l1llll1_opy_ (u"ࠥࡔࡷࡵࡦࡪࡶࠥࠍ"))
-            l111l_opy_.despine()
+            plt.xlabel(l1111ll_opy_ (u"ࠤࡗ࡭ࡲ࡫ࠢࠌ"))
+            plt.ylabel(l1111ll_opy_ (u"ࠥࡔࡷࡵࡦࡪࡶࠥࠍ"))
+            l1lllll_opy_.despine()
             return True
         return False
